4d826bb92d17de003b85bfa263ced607c689fa30
Purushottam <mathadpurushottam@gmail.com>
2024-04-04 21:10:18 +0530

    accordion

---
+++ src/data.js
@@ -0,0 +1,22 @@
+const questions = [
+    {
+      id: 1,
+      title: "Accordion1",
+      info:
+        "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art."
+    },
+    {
+      id: 2,
+      title: "Accordion2",
+      info:
+        "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse."
+    },
+    {
+      id: 3,
+      title: "Accordion3",
+      info:
+        "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial."
+    },
+    
+  ];
+  export default questions;
